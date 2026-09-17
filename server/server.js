cbac61c48526b10d62b54103f32df2e6398315bd
Akshaya-Gampala <akshayagampala11@gmail.com>
2026-09-17 17:10:28 +0530

    Update server configuration

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ const PORT = process.env.PORT || 5000;
 
 // Connect Database and Start Express Server
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT,'0.0.0.0', () => {
     console.log(`====================================================`);
     console.log(`🚀 Server running on port ${PORT}`);
     console.log(`🔗 API Base URL: http://localhost:${PORT}/api`);
